d5ea417c8c58cdb937697faaccf8620dc4194e2f
justjugo <saidmecerrem@gmail.com>
2024-03-07 14:43:51 +0100

    added the main app with the home-page/aboout/contact

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import index,contact,about
+
+urlpatterns = [
+   
+    path('', index,name='index'),
+    path('about/', about,name='about'),
+    path('contact/', contact,name='contact'),
+]
